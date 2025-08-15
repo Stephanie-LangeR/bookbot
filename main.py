74ef54c55aea9f1bc4ad9c9ceff861f627616a77
Stephanie-LangeR <stephanielangerenaud@gmail.com>
2025-08-15 16:16:57 -0400

    Command line headaches

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import sys
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+print(sys.argv[1])
 from stats import get_num_words
 from stats import get_num_char
 from stats import get_sorted_list
@@ -8,18 +12,18 @@ def get_book_text(book_path):
          return read_book
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     text = get_book_text(book_path) # Get the text
     num_words = get_num_words(text) # Pass the text to get_num_words
     char_count = get_num_char(text) # Pass the text to get_num_char
     sorted_list = get_sorted_list(char_count) # Pass the dictionary char_count to get_sorted_list
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print("Analyzing book found at books/*.txt...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
     for item in sorted_list:
         if item["char"].isalpha():
             print(item["char"]+":",item["num"])
-    print(sys.argv[1])
+   
 main() # Call main to start the program
