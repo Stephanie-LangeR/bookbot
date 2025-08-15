53ee57990ed8ede9f45a68321d9fdcac63149799
Stephanie-LangeR <stephanielangerenaud@gmail.com>
2025-08-15 12:13:46 -0400

    One more step to go

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_num_words
 from stats import get_num_char
+from stats import get_sorted_list
 def get_book_text(book_path):
     with open(book_path) as f:
          read_book = f.read()
@@ -10,7 +11,11 @@ def main():
     text = get_book_text(book_path) # Get the text
     num_words = get_num_words(text) # Pass the text to get_num_words
     char_count = get_num_char(text) # Pass the text to get_num_char
+    sorted_list = get_sorted_list(char_count) # Pass the dictionary char_count to get_sorted_list
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"{num_words} words found in the document")
-    print(char_count)
-    
+    print("--------- Character Count -------")
+    print(sorted_list)
 main() # Call main to start the program
--- stats.py
+++ stats.py
@@ -8,3 +8,10 @@ def get_num_char(text):
     lower_case_text = text.lower()
     char_count = Counter(lower_case_text)
     return dict(char_count)
+
+def get_sorted_list(char_count):
+    sorted_list = []
+    for key, value in char_count.items():
+        sorted_list.append({"char" : key, "num" : value})
+    sorted_list.sort(key=lambda i: i["num"], reverse=True)
+    return sorted_list
